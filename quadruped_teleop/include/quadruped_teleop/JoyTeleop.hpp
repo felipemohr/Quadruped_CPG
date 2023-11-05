1251567d4a4db0a4ed5008d958f81e89c8df7b44
Felipe Mohr <felipe18mohr@gmail.com>
2023-11-05 20:57:01 -0300

    Create teleop states and cmd_vel publisher

--- quadruped_teleop/include/quadruped_teleop/JoyTeleop.hpp
+++ quadruped_teleop/include/quadruped_teleop/JoyTeleop.hpp
@@ -3,10 +3,17 @@
 
 #include "rclcpp/rclcpp.hpp"
 #include "sensor_msgs/msg/joy.hpp"
+#include "geometry_msgs/msg/twist.hpp"
 #include "quadruped_kinematics/msg/quadruped_ik.hpp"
 #include "quadruped_teleop/EMAFilter.hpp"
 
 #include <memory>
+
+enum class TeleopState
+{
+  WALKING,
+  MOVING_BODY
+};
 
 class JoyTeleop : public rclcpp::Node
 {
@@ -16,14 +23,21 @@ class JoyTeleop : public rclcpp::Node
 
   private:
     void joyCallback(const sensor_msgs::msg::Joy::SharedPtr msg);
+    void publishVelCallback();
     void publishIKCallback();
+
+    void readVelMsg(const sensor_msgs::msg::Joy::SharedPtr msg);
+    void readIKMsg(const sensor_msgs::msg::Joy::SharedPtr msg);
 
     rclcpp::Subscription<sensor_msgs::msg::Joy>::SharedPtr _joy_subscriber;
 
+    rclcpp::Publisher<geometry_msgs::msg::Twist>::SharedPtr _cmd_vel_publisher;
     rclcpp::Publisher<quadruped_kinematics::msg::QuadrupedIK>::SharedPtr _cmd_ik_publisher;
 
+    rclcpp::TimerBase::SharedPtr _publish_vel_timer;
     rclcpp::TimerBase::SharedPtr _publish_ik_timer;
 
+    geometry_msgs::msg::Twist _vel_msg;
     quadruped_kinematics::msg::QuadrupedIK _ik_msg;
     quadruped_kinematics::msg::QuadrupedIK _ik_msg_filtered;
 
@@ -38,12 +52,18 @@ class JoyTeleop : public rclcpp::Node
     std::map<std::string, uint8_t> _axis_angular_map;
     std::map<std::string, uint8_t> _button_angular_map;
     std::map<std::string, double> _ik_limits;
+    std::map<std::string, double> _vel_limits;
+    uint8_t _change_state_map;
     double _filter_beta;
 
     std::map<std::string, uint8_t> _default_axis_linear_map;
     std::map<std::string, uint8_t> _default_axis_angular_map;
     std::map<std::string, uint8_t> _default_button_angular_map;
     std::map<std::string, double> _default_ik_limits;
+    std::map<std::string, double> _default_vel_limits;
+
+    TeleopState _last_state;
+    TeleopState _state;
 
 };
 
--- quadruped_teleop/src/JoyTeleop.cpp
+++ quadruped_teleop/src/JoyTeleop.cpp
@@ -16,8 +16,10 @@ JoyTeleop::JoyTeleop() : Node("joy_teleop_node")
   _joy_subscriber = this->create_subscription<sensor_msgs::msg::Joy>("joy", 10,
                             std::bind(&JoyTeleop::joyCallback, this, _1));
 
+  _cmd_vel_publisher = this->create_publisher<geometry_msgs::msg::Twist>("cmd_vel", 10);
   _cmd_ik_publisher = this->create_publisher<quadruped_kinematics::msg::QuadrupedIK>("cmd_ik", 10);
 
+  _publish_vel_timer = this->create_wall_timer(20ms, std::bind(&JoyTeleop::publishVelCallback, this));
   _publish_ik_timer = this->create_wall_timer(20ms, std::bind(&JoyTeleop::publishIKCallback, this));
 
   _default_axis_linear_map  = { {"x", 0}, {"y", 1}, {"z", 2} };
@@ -25,6 +27,7 @@ JoyTeleop::JoyTeleop() : Node("joy_teleop_node")
   _default_button_angular_map = { {"roll_positive", 4}, {"roll_negative", 5} };
   _default_ik_limits = { {"x", 0.1}, {"y", 0.1}, {"z", 0.1}, 
                          {"roll", 1.0}, {"pitch", 0.5}, {"yaw", 1.0} };
+  _default_vel_limits = { {"linear_x", 0.5}, {"linear_y", 0.25}, {"angular_z", 1.57} };
 
   _ik_msg.use_feet_transforms = true;
   _ik_msg_filtered.use_feet_transforms = true;
@@ -33,12 +36,16 @@ JoyTeleop::JoyTeleop() : Node("joy_teleop_node")
   this->declare_parameters("axis_angular", _default_axis_angular_map);
   this->declare_parameters("button_angular", _default_button_angular_map);
   this->declare_parameters("ik_limits", _default_ik_limits);
+  this->declare_parameters("vel_limits", _default_vel_limits);
+  this->declare_parameter("change_state", 0);
   this->declare_parameter("filter_beta", 0.9);
 
   this->get_parameters("axis_linear", _axis_linear_map);
   this->get_parameters("axis_angular", _axis_angular_map);
   this->get_parameters("button_angular", _button_angular_map);
   this->get_parameters("ik_limits", _ik_limits);
+  this->get_parameters("vel_limits", _vel_limits);
+  this->get_parameter("change_state", _change_state_map);
   this->get_parameter("filter_beta", _filter_beta);
 
   _body_translation_x.setFilterBeta(_filter_beta);
@@ -48,13 +55,24 @@ JoyTeleop::JoyTeleop() : Node("joy_teleop_node")
   _body_rotation_y.setFilterBeta(_filter_beta);
   _body_rotation_z.setFilterBeta(_filter_beta);
 
+  _last_state = TeleopState::WALKING;
+  _state = TeleopState::WALKING;
+
+  RCLCPP_INFO(this->get_logger(), "Initialized in 'WALKING' state");
 }
 
 JoyTeleop::~JoyTeleop()
 {
 }
 
-void JoyTeleop::joyCallback(const sensor_msgs::msg::Joy::SharedPtr msg)
+void JoyTeleop::readVelMsg(const sensor_msgs::msg::Joy::SharedPtr msg)
+{
+  _vel_msg.linear.x = _vel_limits["linear_x"] * msg->axes[_axis_linear_map["x"]];
+  _vel_msg.linear.y = _vel_limits["linear_y"] * msg->axes[_axis_linear_map["y"]];
+  _vel_msg.angular.z = _vel_limits["angular_z"] * msg->axes[_axis_angular_map["yaw"]];
+}
+
+void JoyTeleop::readIKMsg(const sensor_msgs::msg::Joy::SharedPtr msg)
 {
   _ik_msg.body_translation.x = _ik_limits["x"] * msg->axes[_axis_linear_map["x"]];
   _ik_msg.body_translation.y = _ik_limits["y"] * msg->axes[_axis_linear_map["y"]];
@@ -65,6 +83,32 @@ void JoyTeleop::joyCallback(const sensor_msgs::msg::Joy::SharedPtr msg)
   _ik_msg.body_rotation.y = _ik_limits["pitch"] * ((-msg->axes[_axis_angular_map["pitch_positive"]] + 1)/2 
                                                  - (-msg->axes[_axis_angular_map["pitch_negative"]] + 1)/2);
   _ik_msg.body_rotation.z = _ik_limits["yaw"] * msg->axes[_axis_angular_map["yaw"]];
+}
+
+void JoyTeleop::joyCallback(const sensor_msgs::msg::Joy::SharedPtr msg)
+{
+  if (msg->buttons[_change_state_map] && _state == _last_state)
+  {
+    if (_state == TeleopState::WALKING)
+    {
+      RCLCPP_INFO(this->get_logger(), "State changed to 'MOVING BODY'");
+      _state = TeleopState::MOVING_BODY;
+      _publish_vel_timer->cancel();
+      _publish_ik_timer->reset();
+    }
+    else
+    {
+      RCLCPP_INFO(this->get_logger(), "State changed to 'WALKING'");
+      _state = TeleopState::WALKING;
+      _publish_ik_timer->cancel();
+      _publish_vel_timer->reset();
+    }
+  }
+  else
+  {
+    _state == TeleopState::WALKING ? this->readVelMsg(msg) : this->readIKMsg(msg);
+    _last_state = _state;
+  }
 }
 
 void JoyTeleop::publishIKCallback()
@@ -80,6 +124,10 @@ void JoyTeleop::publishIKCallback()
   _cmd_ik_publisher->publish(_ik_msg_filtered);
 }
 
+void JoyTeleop::publishVelCallback()
+{
+  _cmd_vel_publisher->publish(_vel_msg);
+}
 
 int main(int argc, char **argv)
 {
